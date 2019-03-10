1ec804ca1607350df5e0d61ffabfcf206ba76b16
Naim-Bijapure <Naimbijapure7407@gmail.com>
2019-03-10 19:31:57 +0530

    last modificaton

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -102318,10 +102318,9 @@ var awsmobile = {
     "endpoint": "https://aswacb6ln9.execute-api.us-east-1.amazonaws.com/react",
     "region": "us-east-1"
   }],
-  "aws_cognito_identity_pool_id": "us-east-1:4dcbb790-2792-48e0-8b9f-12b32147f024",
-  "aws_cognito_region": "us-east-1",
-  "aws_user_pools_id": "us-east-1_mmrf9PuD9",
-  "aws_user_pools_web_client_id": "g90p9to483ase07jn9fb91u4n"
+  "aws_content_delivery_bucket": "pronoob-serverless-react",
+  "aws_content_delivery_bucket_region": "us-east-1",
+  "aws_content_delivery_url": "https://d24mo18wp5yd2g.cloudfront.net"
 };
 var _default = awsmobile;
 exports.default = _default;
@@ -112411,7 +112410,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45875" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33023" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
